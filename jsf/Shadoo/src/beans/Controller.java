054cf68f9e3960954073c63df6844bafc2030e99
unknown <mascit25@itpc2210.rznt.rzdir.fht-esslingen.de>
2014-06-18 15:34:22 +0200

    Test Bean

---
+++ jsf/Shadoo/src/beans/Controller.java
@@ -0,0 +1,48 @@
+package beans;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class Controller {
+
+	private String labelFavClass = "btn btn-default btn-favourites";
+	private String labelNewClass = "btn btn-default btn-newest";
+	
+	public void test() {
+		System.out.println("Hallooooooooooooooooooooooooooooooooooooooooooooooooo");
+		
+//		UIViewRoot viewRoot = FacesContext.getCurrentInstance().getViewRoot();
+//		UIComponent component = viewRoot.findComponent("test");
+//		System.out.println(component.getClass());
+		
+//		return "";
+	}
+	
+	public String getLabelFavClass() {
+		return labelFavClass;
+	}
+	public void setLabelFavClass(String labelFavClass) {
+		this.labelFavClass = labelFavClass;
+		System.out.println("favClass: " + labelFavClass);
+	}
+	public String getLabelNewClass() {
+		return labelNewClass;
+	}
+	public void setLabelNewClass(String labelNewClass) {
+		this.labelNewClass = labelNewClass;
+		System.out.println("newClass: " + labelNewClass);
+	}
+	
+}
